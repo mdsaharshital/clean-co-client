348989131fa71a276fdfd72d4d2652a01c511fb8
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-19 06:02:01 +0600

    hero section done

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -1,16 +1,14 @@
 import React from "react";
+import heroImg from "../../assets/images/bucketgirl 1.png";
 
 const Landing = () => {
   return (
-    <div class="hero min-h-screen bg-base-200">
+    <div class="hero min-h-screen bg-accent mt-7">
       <div class="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://api.lorem.space/image/movie?w=260&h=400"
-          class="max-w-sm rounded-lg shadow-2xl"
-          alt=""
-        />
+        <img src={heroImg} class="md:max-w-lg" alt="" />
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <small className="">Best Quality</small>
+          <h1 class="text-5xl font-bold ">Professional Cleaning Service</h1>
           <p class="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
